637b971b3a188e8f2ea2348387846921d2e9d75e
Benjamin Chauhan <benjamin.chauhan@duke.edu>
2026-08-08 14:40:35 -0700

    Re-export the imported-rule types where drivers look for them

--- src/lib.rs
+++ src/lib.rs
@@ -64,8 +64,8 @@ pub mod host;
 pub mod sddp;
 
 pub use host::{
-    Args, Candidate, Connect, DeviceId, DriverModule, Field, HostCall, HttpRequest, Instance,
-    PickRow, Request, Response, SetupStep, dispatch,
+    Args, Candidate, Connect, DeviceId, DriverModule, Field, HostCall, HttpRequest, ImportedAction,
+    ImportedRule, Instance, PickRow, Request, Response, SetupStep, dispatch,
 };
 pub use serde_json::{Value, json};
 
